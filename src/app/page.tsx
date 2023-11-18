999a3370cb1875577fe51773a042ece2eae0e229
devAyham <ayhmhamame14@gmail.com>
2023-11-19 00:53:22 +0300

    feat (src/app) : make redirect from the root path to the dashboard path name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { PagesRotes } from "@/constants/routes/pagesRoutes";
+import { RedirectType, redirect } from "next/navigation";
 
-export default function Home() {
+export default function Page() {
+  redirect(PagesRotes.DashboardRoutes.index, RedirectType.replace);
   return <></>;
 }
