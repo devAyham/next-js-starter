dbd578edf5a93bcb3c679fbe3a608aea8a0599c9
devAyham <ayhmhamame14@gmail.com>
2023-11-19 00:51:00 +0300

    feat (src/app/(content)) : init the folders and structure and the pages for the content features

---
+++ src/app/(content)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page() {
+  return <div>dashbaord </div>;
+}
+
+export default Page;
---
+++ src/app/(content)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function layout({ children }: { children: React.ReactNode }) {
+  return <>test{children}</>;
+}
+
+export default layout;
