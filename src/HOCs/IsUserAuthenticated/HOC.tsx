f98d9ef7b4d1d5882aec3161baab796b94d5c7d5
devAyham <ayhmhamame14@gmail.com>
2023-11-19 22:37:48 +0300

    feat (src/HOCs/IsUserAuthenticated) : get use of the next auth to check if the user is authenticated or not

--- src/HOCs/IsUserAuthenticated/HOC.tsx
+++ src/HOCs/IsUserAuthenticated/HOC.tsx
@@ -1,9 +1,20 @@
-"use client";
+// "use client";
+import { useSession } from "next-auth/react";
 import { Props } from "./Props";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
-function HOC({ Authenticated, NotAuthenticated }: Props) {
+async function  HOC({ Authenticated, NotAuthenticated }: Props) {
+  const session = await getServerSession(options);
+
+  // const { data: session } = useSession({
+  //   required: true,
+  // onUnauthenticated() {
+  //     redirect('/api/auth/signin?callbackUrl=/client')
+  // }
+  // });
   // must check if the user is authenticated in some how
-  return <>{true ? <> {Authenticated}</> : <> {NotAuthenticated}</>}</>;
+  return <>{session ? <> {Authenticated}</> : <> {NotAuthenticated}</>}</>;
 }
 
 export default HOC;
