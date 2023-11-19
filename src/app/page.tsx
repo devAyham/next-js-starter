f0db08139e4675f527cbb35548d9233d8cf7dcab
devAyham <ayhmhamame14@gmail.com>
2023-11-19 22:36:52 +0300

    feat (src/app) : get use of the AuthProvider and the session in the root page and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import { PagesRotes } from "@/constants/routes/pagesRoutes";
 import { RedirectType, redirect } from "next/navigation";
+import { getServerSession } from "next-auth/next";
+import { options } from "./api/auth/[...nextauth]/options";
 
-export default function Page() {
-  redirect(PagesRotes.DashboardRoutes.index, RedirectType.replace);
+export default async function Page() {
+  const session = await getServerSession(options);
+  if (session?.user) {
+    redirect(PagesRotes.DashboardRoutes.index, RedirectType.replace);
+  }
   return <></>;
 }
