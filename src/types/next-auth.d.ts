2d467e05c4a5a847df53d0f16969736781c7533f
devAyham <ayhmhamame14@gmail.com>
2023-11-20 23:45:57 +0300

    feat (src/types) : add new declearetion type for the auth user session

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: number;
+      userName: string;
+      name: string;
+      email: string;
+      accessToken: string;
+      refrshToken: string;
+    };
+  }
+}
