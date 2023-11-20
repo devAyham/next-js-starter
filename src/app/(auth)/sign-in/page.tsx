ccf5ddabaa640f9a1d18ad8aa9beba1b0512008a
devAyham <ayhmhamame14@gmail.com>
2023-11-21 02:46:30 +0300

    feat (src/app/(auth)) : create basic sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
 
-function Page() {
-  return <div>sign in</div>;
+import { signIn } from "next-auth/react";
+import { useRef } from "react";
+
+interface IProps {
+  searchParams?: { [key: string]: string | string[] | undefined };
 }
 
-export default Page;
+const LoginPage = ({ searchParams }: IProps) => {
+  const userName = useRef("");
+  const pass = useRef("");
+
+  const onSubmit = async () => {
+    const result = await signIn("credentials", {
+      username: 'userName.current',
+      password: 'pass.current',
+      redirect: true,
+      callbackUrl: "/",
+    });
+  };
+  return (
+    <div>
+      {searchParams?.message && <p>{searchParams?.message}</p>}
+      <div>
+        <input
+          // lableText="User Name"
+          onChange={(e) => (userName.current = e.target.value)}
+        />
+        <input
+          // lableText="Password"
+          type={"password"}
+          onChange={(e) => (pass.current = e.target.value)}
+        />
+        <button onClick={onSubmit}>Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
