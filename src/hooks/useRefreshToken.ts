3432dc7b9ff1afe39ea18c0f98166f1b2323fb46
devAyham <ayhmhamame14@gmail.com>
2023-11-21 02:45:41 +0300

    feat (src/lib) : create access and refresh tokens strategy using axios interseptor and hooks

---
+++ src/hooks/useRefreshToken.ts
@@ -0,0 +1,26 @@
+"use client";
+
+import axios from "@/lib/axios";
+import { signIn, useSession } from "next-auth/react";
+
+export const useRefreshToken = () => {
+  const { data: session, update } = useSession();
+
+  const refreshToken = async () => {
+    const res = await axios.post("/auth/refresh", {
+      refresh: session?.user.refreshToken,
+    });
+
+    if (session) {
+      session.user.accessToken = res.data.accessToken;
+      update({
+        ...session,
+        user: {
+          ...session?.user,
+          accessToken: res.data.accessToken,
+        },
+      });
+    } else signIn();
+  };
+  return refreshToken;
+};
