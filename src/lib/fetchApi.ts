7d545a3ab7c622fbc9ef6c730ff69696b9e71cfc
devAyham <ayhmhamame14@gmail.com>
2023-11-23 14:03:09 +0300

    feat (src/lib) : creat new function using fetch api to use refresh and access tokens in the server side

--- src/lib/fetchApi.ts
+++ src/lib/fetchApi.ts
@@ -1,45 +1,85 @@
 import { options } from "@/app/api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
-async function refreshToken(refreshToken: string) {
-  const res = await fetch(process.env.BACKEND_BASE_URL + "/auth/refresh", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      refresh: refreshToken,
-    }),
-  });
-  const data = await res.json();
-  console.log({ data });
+import { signOut } from "next-auth/react";
+import { RequestInit } from "next/dist/server/web/spec-extension/request";
 
-  return data.accessToken;
+async function refreshToken(refreshToken: string) {
+  try {
+    const res = await fetch(`http://127.0.0.1:3001/auth/token-by-refresh`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        refresh: refreshToken,
+      }),
+    });
+    if (res.status === 401) {
+      console.log("401");
+      // if (typeof window !== "undefined") {
+      //   await signOut({ callbackUrl: "/login" });
+      // }
+      return null;
+    }
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    // Handle network errors or other exceptions
+    console.error("Error refreshing token:", error);
+    return null;
+  }
 }
 
-export async function AuthGetApi(url: string) {
+export async function request({
+  url,
+  apiConfig,
+}: {
+  url: string;
+  apiConfig?: RequestInit;
+}) {
+  const { headers: myHeaders, ...restConfig } = apiConfig ?? {};
   const session = await getServerSession(options);
-  console.log("before: ", session?.user.accessToken);
+  try {
+    console.log("before: ", session);
 
-  let res = await fetch(process.env.BACKEND_BASE_URL + url, {
-    method: "GET",
-    headers: {
-      Authorization: `bearer ${session?.user.accessToken}`,
-    },
-  });
-
-  if (res.status == 401) {
-    if (session)
-      session.user.accessToken = await refreshToken(
-        session?.user.refreshToken ?? ""
-      );
-    console.log("after: ", session?.user.accessToken);
-
-    res = await fetch(process.env.BACKEND_BASE_URL + url, {
-      method: "GET",
+    let res = await fetch("http://127.0.0.1:3001", {
       headers: {
-        Authorization: `bearer ${session?.user.accessToken}`,
+        Authorization: `Bearer ${session?.tokens.accessToken}`,
+        ...myHeaders,
       },
+      ...restConfig,
     });
-    return await res.json();
-  }
 
-  return await res.json();
+    if (res.status === 401) {
+      if (session) {
+        const newTokens = await refreshToken(session?.tokens.refreshToken);
+        session.tokens.accessToken = newTokens?.accessToken;
+        session.tokens.refreshToken = newTokens?.refreshToken;
+      }
+      // console.log("after: ", session);
+      res = await fetch("http://127.0.0.1:3001", {
+        method: "GET",
+        headers: {
+          Authorization: `bearer ${session?.tokens.accessToken}`,
+          ...myHeaders,
+        },
+        ...restConfig,
+      });
+    }
+
+    if (!res.ok) {
+      throw new Error(`Request failed with status ${res.status}`);
+    }
+
+    return await res.json();
+  } catch (error) {
+    if (session) session.errors = "invalid";
+    // Handle network errors or other exceptions
+    console.error("Error fetching API:", session);
+    // await signOut();
+
+    return null;
+  }
 }
